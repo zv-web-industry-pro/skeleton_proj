99c2c9231de95b0ef97e026294c622c30ec13caa
RabbitInGame <pggnaqope@gmail.com>
2016-07-27 01:16:22 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+$(document).load(function() {
+
+});
+
+$(document).ready(function($) {
+	console.log('JS ready');	
+
+	
+});
+
+
+
+
+$(window).resize(function() {
+	clearTimeout(window.resizedFinished);
+	window.resizedFinished = setTimeout(function(){
+		// исполняемый код тут
+	}, 250);
+});
+
+$(window).scroll(function(event) {
+	// if($(window).scrollTop() > 0){
+	
+	// }
+});
+
+
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var tech = getUrlParameter('technology');
